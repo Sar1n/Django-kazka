3b0d1aa1eb064d8e85e2510161fdabab674dd9af
Maxym Herbich <maxym.herbich@maklai.com.ua>
2020-05-09 17:38:04 +0300

    added isBeingWritten field + methods for admin

--- mysite/Apps/Tales/models.py
+++ mysite/Apps/Tales/models.py
@@ -1,6 +1,7 @@
 import json
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Tale(models.Model):
 	length = models.IntegerField(default=0,	name='Length')
@@ -8,11 +9,18 @@ class Tale(models.Model):
 	lastAuthorID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lastAuthorID')
 	dateStarted = models.DateTimeField(name='dateStarted')
 	dateFinished = models.DateTimeField(name='dateFinished')
-	TaleName = models.TextField(name="TaleName", max_length=50, default="FefaultTaleName")
+	TaleName = models.TextField(name="TaleName", max_length=50, default="defaultTaleName")
+	isBeingWritten = models.BooleanField(default=0, name='isBeingWritten')
+
+	def __str__(self):
+		return self.TaleName
+	def get_absolute_url(self):
+		return reverse('tale', args=[str(self.id)])
+
 
 class Sentence(models.Model):
-	authorID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='AuthorID')
-	taleID = models.ForeignKey(Tale, on_delete=models.CASCADE, related_name='TaleID', )
+	authorID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authorID')
+	taleID = models.ForeignKey(Tale, on_delete=models.CASCADE, related_name='taleID', )
 	dateAdded = models.DateTimeField(name='dateAdded')
 	sentence = models.TextField(name='Sentence', max_length=128)
 
@@ -20,6 +28,9 @@ class Sentence(models.Model):
 		indexes = [
 			models.Index(fields=['taleID','authorID'])
 		]
+
+	def __str__(self):
+		return self.id
 
 #Класс тупо для теста, ибо вставить строку в текущий Tale без объекта User низя
 class TestData(models.Model):
