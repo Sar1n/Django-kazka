f7e49896cc8ef00a60924466a4aa843d6552c476
Maxym Herbich <maxym.herbich@maklai.com.ua>
2020-05-09 17:36:54 +0300

    added display options and filter to admin

--- mysite/Apps/Tales/admin.py
+++ mysite/Apps/Tales/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.unregister(Tale)
+admin.site.unregister(Sentence)
+
+@admin.register(Tale)
+class TaleAdmin(admin.ModelAdmin):
+	list_display = ('id','TaleName', 'lastAuthorID','Length', 'dateStarted', 'dateFinished', 'isFinished', 'isBeingWritten')
+	list_filter = ('id','TaleName', 'lastAuthorID','Length', 'dateStarted', 'dateFinished', 'isFinished', 'isBeingWritten')
+
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id', 'authorID', 'taleID', 'dateAdded', 'Sentence')
+	list_filter = ('id', 'authorID', 'taleID', 'dateAdded', 'Sentence')
