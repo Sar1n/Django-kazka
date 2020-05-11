1bd6f8d24713c0c1143cd56ee552045906f03954
Maxym Herbich <maxym.herbich@maklai.com.ua>
2020-05-11 17:47:37 +0300

    added 4 tales at login page

--- mysite/Apps/Login/views.py
+++ mysite/Apps/Login/views.py
@@ -1,6 +1,6 @@
 import textwrap
 import json
-from mysite.Apps.Login.models import *
+from mysite.Apps.Tales.models import *
 
 from django.http import HttpResponse, Http404
 from django.views.generic.base import View
@@ -10,18 +10,23 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-#def hello(request):
- # return render(request, 'Login_prod.html')
-
 # authentication test >
 def login(request):
-  return render(request, 'Login_prod.html')
+  tales = [""] * 4
+  for i in range (4):
+    dataset = UserlessSentence.objects.all().filter(taleID=i+1)
+    for obj in dataset:
+      tales[i] += obj.Sentence + '. '
+  context = {
+        "first_tale" : tales[0],
+        "second_tale" : tales[1],
+        "third_tale" : tales[2],
+        "fourth_tale" : tales[3],
+    }
+  return render(request, 'Login_prod.html', context)
 
 @login_required
 def home(request):
   #return render(request, 'MainBody.html')
   return redirect('/maingame/', permanent=True)
 # authentication test <
-
-#def redirected_login(request):
- # return redirect()
