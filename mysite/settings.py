21f5a98c227067c4fd1b9cb1f10f770c6f9382b8
Ivan Maluk <ivan.maluk@gmail.com>
2020-06-03 19:32:52 +0300

    add support of images and styles thru django

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', #For images to display
     'social_django',
 ]
 
@@ -83,6 +83,7 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIAL_AUTH_FACEBOOK_KEY = '585270295701492'
 SOCIAL_AUTH_FACEBOOK_SECRET = '54c42be0f83ce1dc87091e40e591bedf'
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
@@ -146,4 +147,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #For images to display
